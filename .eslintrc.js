f262ffaf8a51574b8fdc9f7956a651cc412cf55f
SnowIsTargaryen <chiron1217@naver.com>
2019-04-15 23:58:32 +0900

    gulp,eslint 설정

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        //console,alert eslint 룰 추가
+        "no-console" : "off",
+        "no-alert" : 1
+    }
+};
--- README.md
+++
@@ -1,1 +0,0 @@
-# RunningJS
